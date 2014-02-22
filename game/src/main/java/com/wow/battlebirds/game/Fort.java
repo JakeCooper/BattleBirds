8f462dc7ea89b350bba0259f8a2571e5c67a422a
Andrew Ellis <andrewellis928@gmail.com>
2014-02-22 10:55:36 -0800

    The Fort and the Animation for for destroy

---
+++ game/src/main/java/com/wow/battlebirds/game/Fort.java
@@ -0,0 +1,8 @@
+package com.wow.battlebirds.game;
+
+/**
+ * Created by Andrew on 22/02/14.
+ * The Fort
+ */
+public class Fort {
+}
---
+++ game/src/main/java/com/wow/battlebirds/game/fortCollapse.java
@@ -0,0 +1,8 @@
+package com.wow.battlebirds.game;
+
+/**
+ * Created by Andrew on 22/02/14.
+ * Collapsing Animation for the fort
+ */
+public class fortCollapse {
+}
