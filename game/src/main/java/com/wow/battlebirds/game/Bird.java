aa307f0b3ade884b0ada167a7caa3b24aade1ba1
JakeCooper <jake.elijah.cooper@gmail.com>
2014-02-22 11:09:02 -0800

    Bird

--- game/src/main/java/com/wow/battlebirds/game/Bird.java
+++ game/src/main/java/com/wow/battlebirds/game/Bird.java
@@ -4,12 +4,14 @@ package com.wow.battlebirds.game;
  * Created by Jake on 22/02/14.
  */
 public class Bird {
-    public float VelocityX;
-    public float VelocityY;
+    public float Velocity;
     public int team;
 
     Bird(int team){
-
+        switch (team){
+            case 1: //RENDER BIRD(RED)
+            case 2: //RENDER BIRD(BLUE)
+        }
     }
 
 }
