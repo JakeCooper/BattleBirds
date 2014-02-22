4a67f25ae81e4a2dc4157b9d752e54322498f72d
Chris Hampu <chrishampu@outlook.com>
2014-02-22 10:56:34 -0800

    Added main Activity, View, and Entry point thread There is a basic premise for a main loop, in its own thread. The overridden functions will allow us to control the application state.

---
+++ game/src/main/java/com/wow/battlebirds/game/GameEntry.java
@@ -0,0 +1,39 @@
+package com.wow.battlebirds.game;
+
+import android.view.SurfaceHolder;
+import android.util.Log;
+
+/**
+ * Created by ChrisH on 22/02/14.
+ */
+public class GameEntry extends Thread {
+
+    private SurfaceHolder surfaceHolder;
+    private GameView gamePanel;
+
+    private boolean running;
+
+    public GameEntry(SurfaceHolder surfaceHolder, GameView gamePanel)
+    {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gamePanel = gamePanel;
+    }
+
+    public void setRunning(boolean running)
+    {
+            this.running = running;
+    }
+
+    @Override
+    public void run()
+    {
+        long tickCount = 0L;
+
+        while (running)
+        {
+            tickCount++;
+
+        }
+    }
+}
