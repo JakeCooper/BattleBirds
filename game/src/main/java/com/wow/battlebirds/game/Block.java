42a285e496b15b76a6403b1fae4bfdb9d34fd7f6
Denis Kisselev <denis@dkisselev.net>
2014-02-22 14:37:00 -0800

    Updated block points to Point

--- game/src/main/java/com/wow/battlebirds/game/Block.java
+++ game/src/main/java/com/wow/battlebirds/game/Block.java
@@ -1,6 +1,9 @@
 package com.wow.battlebirds.game;
 
+import android.graphics.Point;
 import android.view.Gravity;
+
+import java.util.ArrayList;
 
 /**
  * Created by Jake on 22/02/14.
@@ -8,26 +11,31 @@ import android.view.Gravity;
 public class Block extends Asset {
     public int length;
     public int height;
-    public int numBlocks = 15;
-    public int xPos;
-    public int yPos;
+    public Point position;
+    public Point[] points;
     public int Vy;
+    public int team;
 
     Block(){
-        if(numBlocks > 0){
-            int length = 20;
-            int height = 20;
-            numBlocks--;
+        this.points = GetPoints();
 
-        } else {
-            System.out.println("Max Blocks Reached");
-        }
         //GET INPUT AND CONVERT TO YPOS AND XPOS!
-        if(yPos < 100) yPos = 100;
-        while(yPos > 100) yPos--; //Dat Gravity
+        if(position.y < 100) position.y = 100;
+        while(position.y > 100) position.y--; //Dat Gravity
 
 
     }/*BlockConstructor*/
+
+    public Point[] GetPoints() {
+        Point Tl = new Point((position.x - (this.length / 2)), (position.y + this.height / 2));
+        Point Tr = new Point((position.x + (this.length / 2)), (position.y + this.height / 2));
+        Point Bl = new Point((position.x - (this.length / 2)), (position.y - this.height / 2));
+        Point Br = new Point((position.x + (this.length / 2)), (position.y - this.height / 2));
+
+        Point[] BlockPoints = {Tl, Tr, Bl, Br};
+
+        return BlockPoints;
+    }
 
 
 
