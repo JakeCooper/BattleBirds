dbf55c25cbd8cc723fb3c28905c29e63aa3c4f93
JakeCooper <jake.elijah.cooper@gmail.com>
2014-02-22 11:51:08 -0800

    Box Gravity

--- game/src/main/java/com/wow/battlebirds/game/Block.java
+++ game/src/main/java/com/wow/battlebirds/game/Block.java
@@ -7,15 +7,24 @@ public class Block {
     public int length;
     public int height;
     public int numBlocks = 15;
+    public int xPos;
+    public int yPos;
+    public int Vy;
+    public float gravity;
 
     Block(){
         if(numBlocks > 0){
             int length = 20;
             int height = 20;
             numBlocks--;
+            while(yPos > 100){
+                Vy += gravity;
+            }
         } else {
             System.out.println("Max Blocks Reached");
         }
+
+
     }/*BlockConstructor*/
 
 
