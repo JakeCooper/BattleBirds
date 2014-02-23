e4284933cef50565cc03b47e015ed3e9c30f2c06
Andrew Ellis <andrewellis928@gmail.com>
2014-02-22 18:48:52 -0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	game/src/main/java/com/wow/battlebirds/game/Cannon.java

--- game/src/main/java/com/wow/battlebirds/game/GameEntry.java
+++ game/src/main/java/com/wow/battlebirds/game/GameEntry.java
@@ -3,9 +3,13 @@ package com.wow.battlebirds.game;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.util.Log;
+import java.util.Queue;
+import android.graphics.Point;
+import java.lang.Math;
 
 /**
  * Created by ChrisH on 22/02/14.
@@ -16,6 +20,7 @@ public class GameEntry extends Thread {
     private GameView gamePanel;
 
     private boolean running;
+    private touchScreen events;
 
     public GameEntry(SurfaceHolder surfaceHolder, GameView gamePanel)
     {
@@ -34,11 +39,58 @@ public class GameEntry extends Thread {
     {
         Rect dstRect = new Rect();
         long startTime = System.nanoTime();
-
+        events = new touchScreen();
         while (running)
         {
             if(!gamePanel.getHolder().getSurface().isValid())
                 continue;
+
+            // Traverses all the current touch events
+
+            Queue<MotionEvent> eventlist = events.getMotionEvents();
+            MotionEvent upEvent;
+            MotionEvent downEvent;
+
+            while(eventlist.peek() != null){
+
+                if(eventlist.peek().getAction() == MotionEvent.ACTION_UP){
+                    upEvent = eventlist.remove();
+                    downEvent = eventlist.remove();
+                    int diff = (int)(upEvent.getRawY() - downEvent.getRawY());
+                    GameScreen screen = (GameScreen)gamePanel.game.getCurrentScreen();
+
+
+                    // Create a new Block
+                    if(downEvent.getRawX() == upEvent.getRawX() || downEvent.getRawY() == upEvent.getRawY() ){
+
+                        Block b = new Block(new Point((int)upEvent.getRawX(), (int)downEvent.getRawY()));
+                        gamePanel.game.getAssetFactory().addAsset(b);
+
+
+                    // Fires the cannon
+                    }else if(new Point((int)downEvent.getRawX(), (int)upEvent.getRawY()) == screen.redCannon.position ){
+                            Bird projectile = new Bird(screen.redCannon.position,20, (int)screen.redCannon.angle);
+                            gamePanel.game.getAssetFactory().addAsset(projectile);
+
+                    // Moves the cannon
+                    }else if(upEvent.getRawX() != downEvent.getRawX()){
+                        // If diff is positive, then increase the angle
+                        for(int i = 0; i < Math.abs(diff); i++){
+                            if(diff > 0){
+                                screen.redCannon.cannonRotation(1);
+                            }else{
+                                screen.redCannon.cannonRotation(-1);
+                            }
+                        }
+                    }
+
+                }else{
+                    continue;
+                }
+
+            }
+
+
 
             float deltaTime = (System.nanoTime() - startTime) / 10000000.000f;
             startTime = System.nanoTime();
